b8db6b2be6a694bb4236a850cc907395e18f3801
Haeretik <haeretik@haeretik.com>
2015-04-04 22:39:56 +0200

    Initial checkin

---
+++ kurs/index.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+<title>PHP Kurs</title>
+</head>
+<body>
+Die erste Zeile in HTML<br />
+<?php echo "Die zweite Zeile in PHP<br />"; ?>
+Die dritte Zeile in HTML<br />
+<?php
+	echo "Die vierte Zeile<br />";
+	echo "Die 5. Zeile<br />";
+?>
+<?php
+	echo "Die 6. Zeile<br />" . "Die 7. Zeile";
+?>
+</body>
+</html>
