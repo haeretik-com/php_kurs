cfd2740fe4777e9d1eaf6d8cb830f4fa2dfb07d3
Haeretik <haeretik@haeretik.com>
2015-04-04 23:09:58 +0200

    Zahlung

--- kurs/index.php
+++ kurs/index.php
@@ -3,15 +3,11 @@
 <title>PHP Kurs</title>
 </head>
 <body>
-Die erste Zeile in HTML<br />
-<?php echo "Die zweite Zeile in PHP<br />"; ?>
-Die dritte Zeile in HTML<br />
 <?php
-	echo "Die vierte Zeile<br />";
-	echo "Die 5. Zeile<br />";
-?>
-<?php
-	echo "Die 6. Zeile<br />" . "Die 7. Zeile";
+	$liter = 14;
+	$preis = 1.35;
+	$zahlung = $liter * $preis;
+	echo $zahlung;
 ?>
 </body>
 </html>
